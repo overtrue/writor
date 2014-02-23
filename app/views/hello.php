78c2537913a68229f9186e5e0a96bc8cd546d164
Joychao <anzhengchao@gmail.com>
2014-02-23 22:38:11 +0800

    修改首页

--- app/views/hello.php
+++ app/views/hello.php
@@ -3,14 +3,13 @@
 <head>
 	<meta charset="UTF-8">
 	<title>Laravel PHP Framework</title>
+	<link href='http://fonts.googleapis.com/css?family=Ubuntu:300italic&subset=latin,latin-ext' rel='stylesheet' type='text/css'>
 	<style>
-		@import url(//fonts.googleapis.com/css?family=Lato:700);
-
 		body {
 			margin:0;
-			font-family:'Lato', sans-serif;
+			font-family: "Helvetica Neue",Helvetica,"Noto Sans","ff-tisa-web-pro-1","ff-tisa-web-pro-2","Lucida Grande","Hiragino Sans GB", "Microsoft YaHei", "WenQuanYi Micro Hei", sans-serif;
 			text-align:center;
-			background: #fefefe;
+			background: #fff;
 			color: #999;
 		}
 
@@ -19,7 +18,7 @@
 			height: 200px;
 			position: absolute;
 			left: 50%;
-			top: 50%;
+			top: 40%;
 			margin-left: -150px;
 			margin-top: -100px;
 		}
@@ -30,9 +29,11 @@
 		}
 
 		h1 {
-			font-size: 32px;
+			font-size: 82px;
 			margin: 16px 0 0 0;
 			color: #000;
+			font-style: italic;
+			font-family: 'Ubuntu', sans-serif;
 		}
 	</style>
 </head>
@@ -40,7 +41,17 @@
 	<div class="welcome">
 		<h1>Writor</h1>
 		<h2>基于Laravel开发的博客系统</h2>
-		<p><a href="/admin" "抢先体验后台">抢先体验后台 -></a> </p>
+		<p>
+			<a href="/admin" "抢先体验后台">抢先体验后台 -></a>
+		</p>
+		<p>
+			<iframe src="http://ghbtns.com/github-btn.html?user=joychao&repo=writor&type=watch"
+  allowtransparency="true" frameborder="0" scrolling="0" width="62" height="20"></iframe>
+			<iframe src="http://ghbtns.com/github-btn.html?user=joychao&repo=writor&type=fork"
+  allowtransparency="true" frameborder="0" scrolling="0" width="62" height="20"></iframe>
+			<iframe src="http://ghbtns.com/github-btn.html?user=joychao&repo=writor&type=follow"
+  allowtransparency="true" frameborder="0" scrolling="0" width="62" height="20"></iframe>
+		</p>
 	</div>
 </body>
 </html>

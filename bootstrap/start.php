e0c27700834294ca14f0a57138d206cb99c16a41
Joychao <anzhengchao@gmail.com>
2014-02-23 18:26:48 +0800

    添加环境

--- bootstrap/start.php
+++ bootstrap/start.php
@@ -26,8 +26,8 @@ $app = new Illuminate\Foundation\Application;
 
 $env = $app->detectEnvironment(array(
 
-	'local' => array('your-machine-name'),
-
+    'local'      => array('*'),
+    'production' => array('server1'),
 ));
 
 /*

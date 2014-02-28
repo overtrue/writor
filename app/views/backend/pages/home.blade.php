e40c632d037644307f126f8684caf5f2b1cb20f9
Joychao <anzhengchao@gmail.com>
2014-02-28 16:30:25 +0800

    调节说明样式

--- app/views/backend/pages/home.blade.php
+++ app/views/backend/pages/home.blade.php
@@ -12,38 +12,38 @@
             <ul>
                 <li>
                 <strong>文章</strong><br>
-                        <label class="pad-x-5">列表:<span class="badge badge-sm badge-success">90%</span></label>
-                        <label class="pad-x-5">发布:<span class="badge badge-sm badge-success">100%</span></label>
-                        <label class="pad-x-5">编辑:<span class="badge badge-sm badge-success">90%</span></label>
-                        <label class="pad-x-5">删除:<span class="badge badge-sm badge-default">0%</span></label>
+                        <label class="pad-x-5">列表: <span class="badge badge-sm badge-success">90%</span></label>
+                        <label class="pad-x-5">发布: <span class="badge badge-sm badge-success">100%</span></label>
+                        <label class="pad-x-5">编辑: <span class="badge badge-sm badge-success">90%</span></label>
+                        <label class="pad-x-5">删除: <span class="badge badge-sm badge-default">0%</span></label>
                 </li>
                 <li>
                 <strong>分类</strong><br>
-                        <label class="pad-x-5">添加:<span class="badge badge-sm badge-success">100%</span></label>
-                        <label class="pad-x-5">编辑:<span class="badge badge-sm badge-default">0%</span></label>
-                        <label class="pad-x-5">删除:<span class="badge badge-sm badge-default">0%</span></label>
+                        <label class="pad-x-5">添加: <span class="badge badge-sm badge-success">100%</span></label>
+                        <label class="pad-x-5">编辑: <span class="badge badge-sm badge-default">0%</span></label>
+                        <label class="pad-x-5">删除: <span class="badge badge-sm badge-default">0%</span></label>
                 </li>
                 <li>
                 <strong>用户</strong><br>
-                        <label class="pad-x-5">添加:<span class="badge badge-sm badge-default">0%</span></label>
-                        <label class="pad-x-5">编辑:<span class="badge badge-sm badge-default">0%</span></label>
-                        <label class="pad-x-5">删除:<span class="badge badge-sm badge-default">0%</span></label>
+                        <label class="pad-x-5">添加: <span class="badge badge-sm badge-default">0%</span></label>
+                        <label class="pad-x-5">编辑: <span class="badge badge-sm badge-default">0%</span></label>
+                        <label class="pad-x-5">删除: <span class="badge badge-sm badge-default">0%</span></label>
                 </li>
                 <li>
                 <strong>链接</strong><br>
-                        <label class="pad-x-5">添加:<span class="badge badge-sm badge-default">0%</span></label>
-                        <label class="pad-x-5">编辑:<span class="badge badge-sm badge-default">0%</span></label>
-                        <label class="pad-x-5">删除:<span class="badge badge-sm badge-default">0%</span></label>
+                        <label class="pad-x-5">添加: <span class="badge badge-sm badge-default">0%</span></label>
+                        <label class="pad-x-5">编辑: <span class="badge badge-sm badge-default">0%</span></label>
+                        <label class="pad-x-5">删除: <span class="badge badge-sm badge-default">0%</span></label>
                 </li>
                 <li>
                 <strong>评论</strong><br>
-                        <label class="pad-x-5">回复:<span class="badge badge-sm badge-default">0%</span></label>
-                        <label class="pad-x-5">删除:<span class="badge badge-sm badge-default">0%</span></label>
+                        <label class="pad-x-5">回复: <span class="badge badge-sm badge-default">0%</span></label>
+                        <label class="pad-x-5">删除: <span class="badge badge-sm badge-default">0%</span></label>
                 </li>
                 <li>
                 <strong>设置</strong><br>
-                        <label class="pad-x-5">基础:<span class="badge badge-sm badge-default">0%</span></label>
-                        <label class="pad-x-5">高级:<span class="badge badge-sm badge-default">0%</span></label>
+                        <label class="pad-x-5">基础: <span class="badge badge-sm badge-default">0%</span></label>
+                        <label class="pad-x-5">高级: <span class="badge badge-sm badge-default">0%</span></label>
                 </li>
             </ul>
         </div>

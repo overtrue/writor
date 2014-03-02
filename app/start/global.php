d56d145ba864380e469716c5cf41198a153126a9
Joychao <anzhengchao@gmail.com>
2014-03-02 12:21:44 +0800

    添加包含路径到app/start/global.php中

--- app/start/global.php
+++ app/start/global.php
@@ -14,7 +14,8 @@
 ClassLoader::addDirectories(array(
 
 	app_path().'/commands',
-	app_path().'/controllers',
+    app_path().'/controllers',
+	app_path().'/library',
 	app_path().'/models',
 	app_path().'/database/seeds',
 

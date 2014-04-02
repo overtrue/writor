fbd05d680fda9f333d45dba7ced7dda8a15554b4
Joychao <anzhengchao@gmail.com>
2014-04-02 17:24:13 +0800

    去掉google字体

--- app/views/backend/pages/login.blade.php
+++ app/views/backend/pages/login.blade.php
@@ -9,7 +9,6 @@
     <title>Writor | 登录</title>
     <link rel="stylesheet" href="{{asset('/backend/js/jquery-ui/css/no-theme/jquery-ui-1.10.3.custom.min.css')}}"  id="style-resource-1">
     <link rel="stylesheet" href="{{asset('/backend/css/font-icons/entypo/css/entypo.css')}}"  id="style-resource-2">
-    <link rel="stylesheet" href="http://fonts.googleapis.com/css?family=Noto+Sans:400,700,400italic"  id="style-resource-3">
     <link rel="stylesheet" href="{{asset('/backend/css/bootstrap.min.css')}}"  id="style-resource-4">
     <link rel="stylesheet" href="{{asset('/backend/css/core.css')}}"  id="style-resource-5">
     <link rel="stylesheet" href="{{asset('/backend/css/forms.css')}}"  id="style-resource-7">

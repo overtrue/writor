12e44898e3fd7ae38505eebd3746b0094185bf88
Joychao <anzhengchao@gmail.com>
2014-04-06 01:41:49 +0800

    删除不用的js文件

--- public/backend/js/admin-common.js
+++ public/backend/js/admin-common.js
@@ -248,17 +248,6 @@ var public_vars = public_vars || {};
     {
       $("textarea.autogrow, textarea.autosize").autosize();
     }
-
-    // iCheck
-    if($.isFunction($.fn.iCheck))
-    {
-      $("input:checkbox").iCheck({
-        checkboxClass: 'icheckbox_minimal',
-        radioClass: 'iradio_minimal',
-        //increaseArea: '20%' // optional
-      });
-    }
-    
     
     // Tagsinput
     if($.isFunction($.fn.tagsinput))
@@ -349,96 +338,7 @@ var public_vars = public_vars || {};
       });
     }
     
-    // Form Validation
-    if($.isFunction($.fn.validate))
-    {
-      $("form.validate").each(function(i, el)
-      {
-        var $this = $(el),
-          opts = {
-            rules: {},
-            messages: {},
-            errorElement: 'span',
-            errorClass: 'validate-has-error',
-            highlight: function (element) {
-              $(element).closest('.form-group').addClass('validate-has-error');
-            },
-            unhighlight: function (element) {
-              $(element).closest('.form-group').removeClass('validate-has-error');
-            },
-            errorPlacement: function (error, element)
-            {
-              if(element.closest('.has-switch').length)
-              {
-                error.insertAfter(element.closest('.has-switch'));
-              }
-              else
-              if(element.parent('.checkbox, .radio').length || element.parent('.input-group').length)
-              {
-                error.insertAfter(element.parent());
-              } 
-              else 
-              {
-                error.insertAfter(element);
-              }
-            }
-          },
-          $fields = $this.find('[data-validate]');
-          
-        $fields.each(function(j, el2)
-        {
-          var $field = $(el2),
-            name = $field.attr('name'),
-            validate = attrDefault($field, 'validate', '').toString(),
-            _validate = validate.split(',');
-          
-          for(var k in _validate)
-          {
-            var rule = _validate[k],
-              params,
-              message;
-            
-            if(typeof opts['rules'][name] == 'undefined')
-            {
-              opts['rules'][name] = {};
-              opts['messages'][name] = {};
-            }
-            
-            if($.inArray(rule, ['required', 'url', 'email', 'number', 'date', 'creditcard']) != -1)
-            {
-              opts['rules'][name][rule] = true;
-              
-              message = $field.data('message-' + rule);
-              
-              if(message)
-              {
-                opts['messages'][name][rule] = message;
-              }
-            }
-            // Parameter Value (#1 parameter)
-            else 
-            if(params = rule.match(/(\w+)\[(.*?)\]/i))
-            {
-              if($.inArray(params[1], ['min', 'max', 'minlength', 'maxlength', 'equalTo']) != -1)
-              {
-                opts['rules'][name][params[1]] = params[2];
-                
-              
-                message = $field.data('message-' + params[1]);
-                
-                if(message)
-                {
-                  opts['messages'][name][params[1]] = message;
-                }
-              }
-            }
-          }
-        });
-        
-        $this.validate(opts);
-      });
-    }
-    
+  
     // Modal Static
     public_vars.$body.on('click', '.modal[data-backdrop="static"]', function(ev)
     {

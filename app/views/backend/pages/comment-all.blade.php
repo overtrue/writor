5ce3f92c7fd427c3923fa8b594aba2821da26df2
Joychao <anzhengchao@gmail.com>
2014-04-06 01:42:30 +0800

    删除不用的js文件

--- app/views/backend/pages/comment-all.blade.php
+++ app/views/backend/pages/comment-all.blade.php
@@ -64,10 +64,8 @@
 
 @section('page_css')
 <link rel="stylesheet" href="{{ asset('/backend/js/selectboxit/jquery.selectBoxIt.css') }}"  id="style-resource-3">
-<link rel="stylesheet" href="{{ asset('/backend/js/icheck/skins/minimal/_all.css') }}"  id="style-resource-5">
 @endsection
 
 @section('page_js')
 <script src="{{ asset('/backend/js/selectboxit/jquery.selectBoxIt.min.js') }}" id="script-resource-11"></script>
-<script src="{{ asset('/backend/js/icheck/icheck.min.js') }}" id="script-resource-18"></script>
 @endsection

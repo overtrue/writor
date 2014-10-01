43272fa302a2f28eb35e2d69f131e0de66658b2d
joychao <anzhengchao@gmail.com>
2014-10-01 10:34:46 +0800

    添加用户可删除性

--- app/listener.php
+++ app/listener.php
@@ -4,9 +4,13 @@ Post::creating(function($post){
     $post->post_author = Auth::user()->id;
 });
 
+//允许用户删除
+User::creating(function($user){
+    $user->deleteable = 1;
+});
 
 //记录SQL
 Event::listen('illuminate.query', function($sql, $param)
 {
     Log::info($sql . ", with[" . join(',', $param) ."]");
-}); 
+});
